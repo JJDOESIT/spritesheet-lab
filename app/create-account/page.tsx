84d7c6084984628426670dbe18bda8d5426ca6e9
Henry Paschke <h3nry.pas@gmail.com>
2024-06-02 13:07:58 -0700

    Added font and pages

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,3 +1,3 @@
 export default function CreateAccount(){
-    return <div>Create</div>
+    return(<div>Create</div>)
 }
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { redditMono } from "./ui/fonts";
+
+
+export default function Page() {
+  return (
+    <div>Home Page</div>
+  );
+}
