a31ed326f79c661f45bcd21485e7374cebcb47c2
James <jamesgwhit@gmail.com>
2024-06-02 13:32:20 -0700

    tailwind

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,3 +1,11 @@
 export default function CreateAccount(){
-    return(<div>Create</div>)
+    return <>
+        <div>
+            <form className="flex flex-col bg-black">
+                <input type="form"></input>
+                <input type="password"></input>
+                <input type="button" formAction="submit"></input>
+            </form>
+        </div>
+    </>
 }
