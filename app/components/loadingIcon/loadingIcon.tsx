bbf9edce1a31d719f8123ada0a2016f85ab3a447
Henry Paschke <h3nry.pas@gmail.com>
2024-06-15 22:18:19 -0700

    Fixed again

--- app/components/loadingIcon/loadingIcon.tsx
+++ app/components/loadingIcon/loadingIcon.tsx
@@ -1,18 +1,21 @@
 "use client";   
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Color } from "three";
 
 
 export default function LoadingIcon({time = 1, tileSize = 1, color, className} : {time?: number, tileSize?: number, color: string, className?: string}) {
     const svg = useRef<SVGSVGElement>(null!);
-    const animations = useRef<SVGElement[]>([]);
-    const intervalId = useRef<NodeJS.Timeout | null>(null);
+    const [visibility, setVisibility] = useState('invisible');
 
     const size = 10;
 
+    useEffect(() => {
+        setVisibility('visible')
+    }, [])
+
     return (
-        <svg ref={svg} viewBox={`${-size} ${-size} ${size*4} ${size*4}`} xmlns="http://www.w3.org/2000/svg"  className={`animate__animated animate__fadeIn animate__slower ${className}`} width={tileSize} height={tileSize}>
+        <svg ref={svg} viewBox={`${-size} ${-size} ${size*4} ${size*4}`} xmlns="http://www.w3.org/2000/svg" className={` animate__animated animate__fadeIn ${visibility} ${className}`} width={tileSize} height={tileSize}>
         <defs/>
         <rect x={-((size/2) + 1)} y={-((size/2) + 1)} width={size} height={size} style={{ fill: color, stroke: color, transformBox: 'fill-box', transformOrigin: '50% 50%' }}>
             <animateTransform type="scale" additive="replace" attributeName="transform" values="0 1;1 1;1 1;0 1;0 1"  dur={`${time}s`} fill="freeze" keyTimes="0; 0.25; 0.5; 0.75; 1" begin="0" repeatCount="indefinite"></animateTransform>
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import SideNav from "./components/sidenav/sidenav";
 import styles from "@/app/layout.module.css";
 import bgStyles from "@/app/home/home.module.css";
 import LoadingIcon from "./components/loadingIcon/loadingIcon";
-import { Color } from "three";
+
 
 export default function RootLayout({
   children,
@@ -21,6 +21,7 @@ export default function RootLayout({
           className={`${styles.heightWithoutTopNav} flex flex-row w-full relative overflow-hidden  ${bgStyles.homeBG}`}
         >
           {children}
+          <LoadingIcon color="black" time={1} tileSize={100} className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
           <SideNav />
         </div>
       </body>
