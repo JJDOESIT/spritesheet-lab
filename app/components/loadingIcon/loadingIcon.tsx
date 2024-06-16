c5518f2e558ad46ec5d6f266d261e51ef313c860
Henry Paschke <h3nry.pas@gmail.com>
2024-06-15 22:01:32 -0700

    Fixed loading animation

--- app/components/loadingIcon/loadingIcon.tsx
+++ app/components/loadingIcon/loadingIcon.tsx
@@ -1,9 +1,10 @@
 "use client";   
 
 import { useEffect, useRef } from "react";
+import { Color } from "three";
 
 
-export default function LoadingIcon({time = 1, tileSize = 1, className} : {time?: number, tileSize?: number, className?: string}) {
+export default function LoadingIcon({time = 1, tileSize = 1, color, className} : {time?: number, tileSize?: number, color: string, className?: string}) {
     const svg = useRef<SVGSVGElement>(null!);
     const animations = useRef<SVGElement[]>([]);
     const intervalId = useRef<NodeJS.Timeout | null>(null);
@@ -11,18 +12,18 @@ export default function LoadingIcon({time = 1, tileSize = 1, className} : {time?
     const size = 10;
 
     return (
-        <svg ref={svg} viewBox={`${-size} ${-size} ${size*4} ${size*4}`} xmlns="http://www.w3.org/2000/svg"  className={className} width={tileSize} height={tileSize}>
+        <svg ref={svg} viewBox={`${-size} ${-size} ${size*4} ${size*4}`} xmlns="http://www.w3.org/2000/svg"  className={`animate__animated animate__fadeIn animate__slower ${className}`} width={tileSize} height={tileSize}>
         <defs/>
-        <rect x={-((size/2) + 1)} y={-((size/2) + 1)} width={size} height={size} style={{ fill: 'rgb(0, 255, 125)', stroke: 'rgb(0, 255, 125)', transformBox: 'fill-box', transformOrigin: '50% 50%' }}>
+        <rect x={-((size/2) + 1)} y={-((size/2) + 1)} width={size} height={size} style={{ fill: color, stroke: color, transformBox: 'fill-box', transformOrigin: '50% 50%' }}>
             <animateTransform type="scale" additive="replace" attributeName="transform" values="0 1;1 1;1 1;0 1;0 1"  dur={`${time}s`} fill="freeze" keyTimes="0; 0.25; 0.5; 0.75; 1" begin="0" repeatCount="indefinite"></animateTransform>
         </rect>
-        <rect x={((size/2) + 1)} y={-((size/2) + 1)} width={size} height={size} style={{ fill: 'rgb(0, 255, 125)', stroke: 'rgb(0, 255, 125)', transformBox: 'fill-box', transformOrigin: '50% 50%' }}>
+        <rect x={((size/2) + 1)} y={-((size/2) + 1)} width={size} height={size} style={{ fill: color, stroke: color, transformBox: 'fill-box', transformOrigin: '50% 50%' }}>
             <animateTransform type="scale" additive="replace" attributeName="transform" values="1 0;1 0;1 1;1 1;1 0" dur={`${time}s`} fill="freeze" keyTimes="0; 0.25; 0.5; 0.75; 1" begin="0" repeatCount="indefinite"></animateTransform>
         </rect>
-        <rect x={((size/2) + 1)} y={((size/2) + 1)} width={size} height={size} style={{ fill: 'rgb(0, 255, 125)', stroke: 'rgb(0, 255, 125)', transformBox: 'fill-box', transformOrigin: '50% 50%' }}>
+        <rect x={((size/2) + 1)} y={((size/2) + 1)} width={size} height={size} style={{ fill: color, stroke: color, transformBox: 'fill-box', transformOrigin: '50% 50%' }}>
         <animateTransform type="scale" additive="replace" attributeName="transform" values="1 1;0 1;0 1;1 1;1 1" dur={`${time}s`} fill="freeze" keyTimes="0; 0.25; 0.5; 0.75; 1" begin="0" repeatCount="indefinite"></animateTransform>
         </rect>
-        <rect x={-((size/2) + 1)} y={((size/2) + 1)} width={size} height={size} style={{ fill: 'rgb(0, 255, 125)', stroke: 'rgb(0, 255, 125)', transformBox: 'fill-box', transformOrigin: '50% 50%' }}>
+        <rect x={-((size/2) + 1)} y={((size/2) + 1)} width={size} height={size} style={{ fill: color, stroke: color, transformBox: 'fill-box', transformOrigin: '50% 50%' }}>
             <animateTransform type="scale" additive="replace" attributeName="transform" values="1 1;1 1;1 0;1 0;1 1" dur={`${time}s`} fill="freeze" keyTimes="0; 0.25; 0.5; 0.75; 1" begin="0" repeatCount="indefinite"></animateTransform>
         </rect>
         </svg>
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import SideNav from "./components/sidenav/sidenav";
 import styles from "@/app/layout.module.css";
 import bgStyles from "@/app/home/home.module.css";
 import LoadingIcon from "./components/loadingIcon/loadingIcon";
+import { Color } from "three";
 
 export default function RootLayout({
   children,
@@ -21,7 +22,7 @@ export default function RootLayout({
         <div
           className={`${styles.heightWithoutTopNav} flex flex-row w-full relative overflow-hidden  ${bgStyles.homeBG}`}
         >
-          <LoadingIcon time={1} tileSize={80} className="absolute top-1/2 left-1/2"/>
+          <LoadingIcon time={1} tileSize={80} color="RGB(0,255,125)" className="absolute top-1/2 left-1/2"/>
           {children}
           <SideNav />
         </div>
