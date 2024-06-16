2628dfcb54721a48a3e102a9be899b7e2638723d
Henry Paschke <h3nry.pas@gmail.com>
2024-06-15 20:02:25 -0700

    Fixed issues

--- app/components/sidenav/sidenav.tsx
+++ app/components/sidenav/sidenav.tsx
@@ -117,34 +117,6 @@ export default function SideNav() {
       </Link>
       <Link
         href={"/contact"}
-        className={`neonBlackButton ${styles.sideNavButton}`}
-      >
-        <EnvelopeIcon className={styles.sideNavButtonIcon} />
-        Contact us
-      </Link>
-      <div className={styles.sideNavBar}>Social</div>
-
-      <Link href={"/"} className={`neonBlackButton ${styles.sideNavButton}`}>
-        <BellIcon className={styles.sideNavButtonIcon} />
-        Notifications
-      </Link>
-      <Link href={"/"} className={`neonBlackButton ${styles.sideNavButton}`}>
-        <ChatBubbleLeftRightIcon className={styles.sideNavButtonIcon} />
-        Messages
-      </Link>
-
-      <div className={styles.sideNavBar}>Info</div>
-
-      <Link href={"/"} className={`neonBlackButton ${styles.sideNavButton}`}>
-        <DocumentTextIcon className={styles.sideNavButtonIcon} />
-        Licensing
-      </Link>
-      <Link href={"/"} className={`neonBlackButton ${styles.sideNavButton}`}>
-        <QuestionMarkCircleIcon className={styles.sideNavButtonIcon} />
-        About us
-      </Link>
-      <Link
-        href={"/contact"}
         onClick={handleClick}
         className={`neonBlackButton ${styles.sideNavButton}`}
       >
